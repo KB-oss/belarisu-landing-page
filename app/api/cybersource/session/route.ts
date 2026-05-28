11c2afc0acf54c6ca8824b9886982c28a88c996c
KB-oss <kibromspr8@gmail.com>
2026-05-28 16:27:54 +0300

    fix: update url env

--- app/api/cybersource/session/route.ts
+++ app/api/cybersource/session/route.ts
@@ -12,11 +12,11 @@ export async function POST(req: NextRequest) {
         const secretKey = process.env.CYBERSOURCE_SECRET_KEY!;
         const host = 'apitest.cybersource.com';
 
-        // const targetOrigin = process.env.NEXT_PUBLIC_APP_URL
+        const targetOrigin = process.env.NEXT_PUBLIC_CYBER_SOURCE_APP_URL
         const formattedAmount = amount.toFixed(2);
 
         const requestBody = {
-            targetOrigins: ['https://joaquin-frenular-daylily.ngrok-free.dev'],
+            targetOrigins: [targetOrigin],
             clientVersion: "1.0",
             allowedCardNetworks: ["VISA", "MASTERCARD"],
             allowedPaymentTypes: ["PANENTRY"],
