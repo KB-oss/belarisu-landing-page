3385016472243f7607061f33580c4760b91e3375
KB-oss <kibromspr8@gmail.com>
2026-05-28 16:50:58 +0300

    fix: session

--- app/cyper/page.tsx
+++ app/cyper/page.tsx
@@ -33,7 +33,6 @@ export default function DonatePage() {
                 method: 'POST',
                 headers: { 
                     'Content-Type': 'application/json',
-                    'ngrok-skip-browser-warning': '69420'
                  },
                 
                 body: JSON.stringify({ amount: numAmount })
