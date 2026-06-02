ba20f7b8f70294257b28fb4b1283486a1551c5f1
KB-oss <kibromspr8@gmail.com>
2026-06-02 16:11:53 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -15,17 +15,17 @@ const nextConfig: NextConfig = {
   },
   async headers() {
     return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "*"
-          }
-        ]
-      }
+        {
+            source: '/:path*',
+            headers: [
+                {
+                    key: 'Content-Security-Policy',
+                    value: "default-src * 'unsafe-inline' 'unsafe-eval' 'unsafe-hashes' 'wasm-unsafe-eval' data: blob:; script-src * 'unsafe-inline' 'unsafe-eval' 'unsafe-hashes' 'wasm-unsafe-eval'; style-src * 'unsafe-inline' 'unsafe-hashes'; img-src * data: blob:; font-src * data:; frame-src *; connect-src *; media-src *; object-src *; worker-src *; frame-ancestors *; base-uri *; form-action *"
+                }
+            ]
+        }
     ];
-  },
+},
 };
 
 export default nextConfig;
