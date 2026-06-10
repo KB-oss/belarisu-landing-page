3a759e22147af4554675dedf720942684a1ec289
KB-oss <kibromspr8@gmail.com>
2026-06-10 21:28:50 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/:path*',
+                source: '/cyper',
                 headers: [
                     {
                         key: 'Content-Security-Policy',
