13031a0d17b07d8b3e13ca1da68fabc748cfbe42
KB-oss <kibromspr8@gmail.com>
2026-07-15 14:15:31 +0300

    fix: add new headers

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig = {
                 headers: [
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.cybersource.com https://*.online-metrix.net https://*.cardinalcommerce.com https://*.googleapis.com https://*.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' https://*.cybersource.com https://*.online-metrix.net https://*.cardinalcommerce.com; connect-src 'self' https://*.cybersource.com https://*.online-metrix.net https://*.cardinalcommerce.com; img-src 'self' data: https://*.cybersource.com https://*.online-metrix.net"
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://setup.cybersource.com https://testflex.cybersource.com https://*.cybersource.com https://*.online-metrix.net https://*.cardinalcommerce.com https://*.googleapis.com https://*.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.cybersource.com; font-src 'self' data: https://fonts.gstatic.com https://*.cybersource.com; frame-src 'self' https://*.cybersource.com https://*.online-metrix.net https://*.cardinalcommerce.com; connect-src 'self' https://*.cybersource.com https://*.online-metrix.net https://*.cardinalcommerce.com; img-src 'self' data: https://*.cybersource.com https://*.online-metrix.net"
                     }
                 ]
             }
